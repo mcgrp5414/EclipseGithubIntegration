07bab017c767637274dded97c53b498b9cfcac8c
mcgrp5414 <prasanth.musala87@gmail.com>
2023-06-02 20:50:31 +0530

    "First cummit"

---
+++ src/SamplePackage/Class1.java
@@ -0,0 +1,18 @@
+package SamplePackage;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		
+		Boolean Hungry=false;;
+		
+		if(Hungry==true) 
+		{
+			System.out.println("Eat something");
+		}
+		else
+		System.out.println("No need to eat");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 003D2H744
+ *
+ */
+module EclipseIntegrationGithubV1 {
+}
